609a92b863e5acf10746c152109bd14f0e45768f
NisanStha <59771589+NisanStha@users.noreply.github.com>
2020-02-11 21:47:21 +0545

    Add files via upload

--- debug.c
+++ debug.c
@@ -2,29 +2,27 @@
 #include<conio.h>
 #include <Windows.h>
 #define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+struct sub  {
+                int type;
+            };
 
-struct master
-{
-    int x,y;
-    struct sub mini;
-};
-struct sub
-{
-    int type;
-
-};
+struct master{
+                int x,y;
+                struct sub mini;
+             };
 
 void main()
-{   struct master mega;
-    mega.x=0;
-    mega.y=1;
+{   struct master mega[10][10];
+    mega[1][1].x=10;
+    mega[1][1].y=10;
     printf("hello world");
-    printf("%d %d  \n", mega.x, mega.y);
-    fun(&mega);
-
+    printf("%d %d  \n", mega[1][1].x, mega[1][1].y);
+    fun(mega);
+    printf("%d %d  \n", mega[1][1].x, mega[1][1].y);
 }
 
-void fun(struct master *mega)
+void fun(struct master meg[][10])
 {
-    printf("%d %d", mega->x, mega->y);
+    printf("%d %d\n", meg[1][1].x, meg[1][1].y);
+    meg[1][1].y=20;
 }
